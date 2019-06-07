93c7469c6a856265b5539c92113665118bf7ead9
Bodgar <bodgar_>
2019-06-06 21:21:00 -0500

    Nuevo

---
+++ app/src/main/java/com/appfitnessapp/app/fitnessapp/Usuario/Calificar.java
@@ -0,0 +1,60 @@
+package com.appfitnessapp.app.fitnessapp.Usuario;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
+import android.widget.Spinner;
+
+import com.appfitnessapp.app.fitnessapp.R;
+
+public class Calificar extends AppCompatActivity {
+
+    RatingBar ratingValoracion;
+    EditText edtExperiencia;
+    LinearLayout btnFotoAntes,btnFotoDespues,btnCalificar;
+    Spinner spinnerPeso;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.usuario_24_calificar);
+
+        ratingValoracion=findViewById(R.id.Valoracion);
+
+        edtExperiencia=findViewById(R.id.edtExperiencia);
+
+        btnFotoAntes=findViewById(R.id.btnFotoAntes);
+        btnFotoDespues=findViewById(R.id.btnFotoDespues);
+        btnCalificar=findViewById(R.id.linearCalificar);
+
+        spinnerPeso=findViewById(R.id.spinnerPeso);
+
+        btnFotoAntes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        btnFotoDespues.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnCalificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/appfitnessapp/app/fitnessapp/Usuario/EditarPerfil.java
@@ -0,0 +1,15 @@
+package com.appfitnessapp.app.fitnessapp.Usuario;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.appfitnessapp.app.fitnessapp.R;
+
+public class EditarPerfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.usuario_22_editar_perfil);
+    }
+}
